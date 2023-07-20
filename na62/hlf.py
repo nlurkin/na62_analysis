59715206ac5be2b09b54566603b466617e826bbb
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-07-20 15:04:08 +0200

    Split sum_three_momenta into two functions.
    One to compute the sum three momentum, and one to compute the magnitude

--- na62/hlf.py
+++ na62/hlf.py
@@ -16,17 +16,17 @@ def total_momentum(df: pd.DataFrame) -> pd.Series:
     t3 = track(df, 3).fillna(0)
     c1 = photon_momentum(df, 1).fillna(0)
     c2 = photon_momentum(df, 2).fillna(0)
-    return sum_three_momenta([t1, t2, t3, c1, c2])
+    return three_vector_mag(sum_three_momenta([t1, t2, t3, c1, c2]))
 
 
 def total_track_momentum(df: pd.DataFrame) -> pd.Series:
     t1 = track(df, 1).fillna(0)
     t2 = track(df, 2).fillna(0)
     t3 = track(df, 3).fillna(0)
-    return sum_three_momenta([t1, t2, t3])
+    return three_vector_mag(sum_three_momenta([t1, t2, t3]))
 
 
-def sum_three_momenta(momenta: List[pd.DataFrame]) -> pd.Series:
+def sum_three_momenta(momenta: List[pd.DataFrame]) -> pd.DataFrame:
     if len(momenta) == 0:
         return pd.Series()
 
@@ -39,6 +39,16 @@ def sum_three_momenta(momenta: List[pd.DataFrame]) -> pd.Series:
         y += p["direction_y"]*p["momentum_mag"]
         z += p["direction_z"]*p["momentum_mag"]
 
+    mag = np.sqrt(x**2 + y**2 + z**2)
+    x /= mag
+    y /= mag
+    z /= mag
+    return pd.DataFrame({"direction_x": x, "direction_y": y, "direction_z": z, "momentum_mag": mag})
+
+def three_vector_mag(vector: pd.DataFrame) -> pd.Series:
+    x = vector["direction_x"]*vector["momentum_mag"]
+    y = vector["direction_y"]*vector["momentum_mag"]
+    z = vector["direction_z"]*vector["momentum_mag"]
     return np.sqrt(x**2 + y**2 + z**2)
 
 
