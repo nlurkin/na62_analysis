6482184577ef31d44a09556d3d903096c5fff7c3
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-07-20 15:44:53 +0200

    Add particle masses as constants

--- na62/constants.py
+++ na62/constants.py
@@ -1,1 +1,9 @@
+# Detector related constants
 lkr_position = 241093
+
+# Particle properties
+muon_mass = 105.7
+kaon_charged_mass = 493.677
+pion_charged_mass = 139.57039
+pion_neutral_mass = 134.9768
+
