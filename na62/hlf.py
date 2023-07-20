88daccd7f97a070e287dc330fd95437023eb72fb
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-07-20 15:51:12 +0200

    Add functions for four-vector and missing mass

--- na62/hlf.py
+++ na62/hlf.py
@@ -45,11 +45,50 @@ def sum_three_momenta(momenta: List[pd.DataFrame]) -> pd.DataFrame:
     z /= mag
     return pd.DataFrame({"direction_x": x, "direction_y": y, "direction_z": z, "momentum_mag": mag})
 
+
 def three_vector_mag(vector: pd.DataFrame) -> pd.Series:
     x = vector["direction_x"]*vector["momentum_mag"]
     y = vector["direction_y"]*vector["momentum_mag"]
     z = vector["direction_z"]*vector["momentum_mag"]
     return np.sqrt(x**2 + y**2 + z**2)
+
+
+def missing_mass_sqr(beam: pd.DataFrame, tracks: List[pd.DataFrame]) -> pd.Series:
+    beam_x = beam["direction_x"]*beam["momentum_mag"]
+    beam_y = beam["direction_y"]*beam["momentum_mag"]
+    beam_z = beam["direction_z"]*beam["momentum_mag"]
+    beam_e = beam["energy"]
+
+    decay_momentum_sum = sum_four_momenta(tracks)
+    decay_x = decay_momentum_sum["direction_x"] * \
+        decay_momentum_sum["momentum_mag"]
+    decay_y = decay_momentum_sum["direction_y"] * \
+        decay_momentum_sum["momentum_mag"]
+    decay_z = decay_momentum_sum["direction_z"] * \
+        decay_momentum_sum["momentum_mag"]
+    decay_e = decay_momentum_sum["energy"]
+
+    return (beam_e - decay_e)**2 - (beam_x - decay_x)**2 - (beam_y - decay_y)**2 - (beam_z - decay_z)**2
+
+
+def missing_mass(beam: pd.DataFrame, tracks: List[pd.DataFrame]) -> pd.Series:
+    mm2 = missing_mass_sqr(beam, tracks)
+
+    return np.sign(mm2)*np.sqrt(np.abs(mm2))
+
+
+def sum_four_momenta(momenta: List[pd.DataFrame]) -> pd.DataFrame:
+    if len(momenta) == 0:
+        return pd.Series()
+
+    momentum_sum = sum_three_momenta(momenta)
+
+    p = momenta[0]
+    momentum_sum["energy"] = p["energy"]
+    for p in momenta[1:]:
+        momentum_sum["energy"] += p["energy"]
+
+    return momentum_sum
 
 
 def lkr_energy(df: pd.DataFrame) -> pd.Series:
