a8946d5d4a963f7221ab843d29d957568fca35ef
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-07-26 15:06:41 +0200

    Add stats.py module, containing function for gaussian fit

---
+++ na62/stats.py
@@ -0,0 +1,40 @@
+from typing import Tuple, Union
+
+import lmfit
+import numpy as np
+import pandas as pd
+from lmfit.models import GaussianModel
+from matplotlib import pyplot as plt
+
+
+def fit_gaussian(data: pd.DataFrame, *, bins: int, display_range: Tuple[int, int], fit_range: Union[Tuple[int, int], None] = None, ax: Union[None, plt.Axes] = None, plot: bool = False) -> lmfit.model.ModelResult:
+    # Compute the correct range and binning
+    if fit_range is None:
+        fit_range = display_range
+    drange_size = display_range[1]-display_range[0]
+    frange_size = fit_range[1] - fit_range[0]
+    fit_bins = bins
+    if drange_size != frange_size:
+        fit_bins = int(bins*frange_size/drange_size)
+
+    # Histogramming and bin centers (assuming constant binning)
+    h, bins = np.histogram(data, bins=fit_bins, range=fit_range)
+    bin_size = bins[1]-bins[0]
+    bins_center = np.array([_ + bin_size/2 for _ in bins[:-1]])
+
+    # Performing the fit
+    gmodel = GaussianModel()
+    pars = gmodel.guess(h, x=bins_center)
+    out = gmodel.fit(h, pars, x=bins_center)
+
+    # Drawing if needed
+    if ax is not None:
+        plot = True
+
+    if plot:
+        if ax is None:
+            ax = plt.figure().gca()
+        data.hist(bins=100, range=(65000, 85000), ax=ax)
+        ax.plot(bins_center, out.best_fit, "-", label="Fit")
+        ax.legend()
+    return out
