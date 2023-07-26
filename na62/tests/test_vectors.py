285f8bf3bca12e506121840e35b380747b98c318
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-07-26 16:32:30 +0200

    Formatting of test file

--- na62/tests/test_vectors.py
+++ na62/tests/test_vectors.py
@@ -1,11 +1,14 @@
-import pytest
 import numpy as np
 import pandas as pd
+import pytest
+
 from .. import hlf
+
 
 def get_unit_magnitude(vector):
     ''' Returns the magnitude of of the direction vector'''
     return np.sqrt(vector["direction_x"]**2 + vector["direction_y"]**2 + vector["direction_z"]**2)
+
 
 class Test_ThreeVector:
     # Functions being tested
@@ -54,28 +57,28 @@ class Test_ThreeVector:
         return pd.DataFrame({
             "direction_x": [0.60445218, 0.66373376, 0.57286913, 0.64398297, 0.42157816, 0.20825984, 0.75285458, 0.49409809, 0.88044839, 0.70334033],
             "direction_y": [0.55179659, 0.39065836, 0.81899179, 0.48974082, 0.55218452, 0.61595781, 0.39146875, 0.82625722, 0.27300523, 0.37574975],
-            "direction_z": [0.57459384, 0.63784288, 0.03276302, 0.58774132, 0.71928027, 0.75975247, 0.52911455, 0.2704923 , 0.38765807, 0.6034273],
-            "momentum_mag": [ 55733.71152199,  31955.95888169,  18957.22794326,  59592.03196925, 117784.35646802,  52734.29043842,  99537.85438574,  32116.17093987, 20473.60615187,  66986.42759534]})
+            "direction_z": [0.57459384, 0.63784288, 0.03276302, 0.58774132, 0.71928027, 0.75975247, 0.52911455, 0.2704923, 0.38765807, 0.6034273],
+            "momentum_mag": [55733.71152199,  31955.95888169,  18957.22794326,  59592.03196925, 117784.35646802,  52734.29043842,  99537.85438574,  32116.17093987, 20473.60615187,  66986.42759534]})
 
     def test_sum(self, vector1, vector2, vector_sum):
         ''' Test the sum function. The value provided by the sum_function need to be very close from the known sum '''
         sum = Test_ThreeVector.sum_function([vector1, vector2])
-        assert(np.isclose(sum, vector_sum).all().all())
+        assert (np.isclose(sum, vector_sum).all().all())
 
     def test_sum_is_unit(self, vector1, vector2):
         ''' Test the sum function. Make sure that the direction of the returned vector is unit '''
         sum = Test_ThreeVector.sum_function([vector1, vector2])
-        assert(np.isclose(get_unit_magnitude(sum), 1).all())
+        assert (np.isclose(get_unit_magnitude(sum), 1).all())
 
     def test_magnitude(self, vector1):
         ''' Test the magnitude function. The value provided by the mag_function need to be very close from the known magnitude'''
         mag = Test_ThreeVector.mag_function(vector1)
-        assert(np.isclose(mag, vector1["momentum_mag"]).all())
+        assert (np.isclose(mag, vector1["momentum_mag"]).all())
 
     def test_unit_direction(self, vector1):
         ''' Test that the given direction vector is unit '''
         mag = get_unit_magnitude(vector1)
-        assert(np.isclose(mag, 1).all())
+        assert (np.isclose(mag, 1).all())
 
     def run_tests(self, sum_function, mag_function):
         ''' Run the tests manually, comparing the results on randomly generated vectors against the library functions '''
@@ -86,16 +89,19 @@ class Test_ThreeVector:
         v2 = Test_ThreeVector.generate_vector()
         vsum = hlf.three_vectors_sum([v1, v2])
         failed = False
+
         try:
             self.test_magnitude(v1)
         except AssertionError:
             print("[ERROR] Magnitude function does not return the expected values")
             failed = True
+
         try:
             self.test_sum(v1, v2, vsum)
         except AssertionError:
             print("[ERROR] Sum function does not return the expected values")
             failed = True
+
         try:
             self.test_sum_is_unit(v1, v2)
         except AssertionError:
@@ -104,6 +110,7 @@ class Test_ThreeVector:
 
         if not failed:
             print("[INFO] All tests passed successfully")
+
 
 class Test_FourVector:
     # Functions to be tested
@@ -137,8 +144,10 @@ class Test_FourVector:
     # All vectors below and randomly generated do not use imaginary masses, but instead the convention that negative mass squared have also a negative mass
     @pytest.fixture
     def vector1(self):
-        mass = np.array([-125.86370468, -10.00444941, -91.09354235, 26.34944601, -95.34073029, -68.05032306, 85.79016754, 150.30090188, 35.31449635, -9.57730493])
-        mag = np.array([89816.97911083, 19969.53432883, 76530.7543659, 12017.98030454, 95353.74551885, 76431.69768715, 67028.67134491, 74672.46256574, 8448.12977028, 74339.24455401])
+        mass = np.array([-125.86370468, -10.00444941, -91.09354235, 26.34944601, -
+                        95.34073029, -68.05032306, 85.79016754, 150.30090188, 35.31449635, -9.57730493])
+        mag = np.array([89816.97911083, 19969.53432883, 76530.7543659, 12017.98030454, 95353.74551885,
+                       76431.69768715, 67028.67134491, 74672.46256574, 8448.12977028, 74339.24455401])
         return pd.DataFrame({
             "direction_x": [0.540903, 0.650243, 0.870689, 0.652073, 0.726161, 0.075428, 0.609854, 0.514185, 0.839974, 0.153135],
             "direction_y": [0.565792, 0.391276, 0.345009, 0.400395, 0.218585, 0.313102, 0.428771, 0.823919, 0.370508, 0.513821],
@@ -149,8 +158,10 @@ class Test_FourVector:
 
     @pytest.fixture
     def vector2(self):
-        mass = np.array([30.64407434, -37.01375581, 167.11938025, -182.46611349, -63.38642282, 86.35085435, -7.76572137, 133.78159979, 16.17130092, -133.88892786])
-        mag = np.array([61051.04084236, 15071.86750623, 55553.63921759, 29208.78823655, 6008.00434838, 68331.94055384, 92244.26649603, 17749.26573943, 37217.42032166, 56900.02439955])
+        mass = np.array([30.64407434, -37.01375581, 167.11938025, -182.46611349, -
+                        63.38642282, 86.35085435, -7.76572137, 133.78159979, 16.17130092, -133.88892786])
+        mag = np.array([61051.04084236, 15071.86750623, 55553.63921759, 29208.78823655, 6008.00434838,
+                       68331.94055384, 92244.26649603, 17749.26573943, 37217.42032166, 56900.02439955])
         return pd.DataFrame({
             "direction_x": [0.853424, 0.830252, 0.555158, 0.503505, 0.201706, 0.347256, 0.817377, 0.127174, 0.934739, 0.851988],
             "direction_y": [0.432551, 0.366020, 0.831525, 0.832755, 0.695402, 0.894849, 0.364707, 0.721017, 0.045276, 0.280187],
@@ -171,22 +182,23 @@ class Test_FourVector:
     def test_sum(self, vector1, vector2, vector_sum):
         ''' Test the sum function. The value provided by the sum_function need to be very close from the known sum '''
         sum = Test_FourVector.sum_function([vector1, vector2])
-        assert(np.isclose(sum, vector_sum).all().all())
+        assert (np.isclose(sum, vector_sum).all().all())
 
     def test_sum_is_unit(self, vector1, vector2):
         ''' Test the sum function. Make sure that the direction of the returned vector is unit '''
         sum = Test_FourVector.sum_function([vector1, vector2])
-        assert(np.isclose(get_unit_magnitude(sum), 1).all())
+        assert (np.isclose(get_unit_magnitude(sum), 1).all())
 
     def test_magnitude(self, vector1):
         ''' Test the magnitude function. The value provided by the mag_function need to be very close from the known magnitude'''
         mag = Test_FourVector.mag_function(vector1)
-        assert(np.isclose(mag, vector1["mass"]).all())
+        assert (np.isclose(mag, vector1["mass"]).all())
 
     def test_magnitude2(self, vector1):
         ''' Test the magnitude squared function. The value provided by the mag2_function need to be very close from the known magnitude squared'''
         mag = Test_FourVector.mag2_function(vector1)
-        assert(np.isclose(mag, np.sign(vector1["mass"])*vector1["mass"]**2).all())
+        assert (np.isclose(mag, np.sign(
+            vector1["mass"])*vector1["mass"]**2).all())
 
     def run_tests(self, sum_function, mag_function, mag2_function):
         ''' Run the tests manually, comparing the results on randomly generated vectors against the library functions '''
@@ -198,21 +210,26 @@ class Test_FourVector:
         v2 = Test_FourVector.generate_vector()
         vsum = hlf.four_vector_sum([v1, v2])
         failed = False
+
         try:
             self.test_magnitude2(v1)
         except AssertionError:
-            print("[ERROR] Magnitude squared function does not return the expected values")
+            print(
+                "[ERROR] Magnitude squared function does not return the expected values")
             failed = True
+
         try:
             self.test_magnitude(v1)
         except AssertionError:
             print("[ERROR] Magnitude function does not return the expected values")
             failed = True
+
         try:
             self.test_sum(v1, v2, vsum)
         except AssertionError:
             print("[ERROR] Sum function does not return the expected values")
             failed = True
+
         try:
             self.test_sum_is_unit(v1, v2)
         except AssertionError:
