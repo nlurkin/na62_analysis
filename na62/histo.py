90631765f71a4dee414386394c5cb01ec086bdd0
Nicolas Lurkin <nicolas.lurkin@uclouvain.be>
2023-08-09 16:39:26 +0200

    Add function hist_data
    Create histogram with error bars

--- na62/histo.py
+++ na62/histo.py
@@ -4,3 +4,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
+def get_bin_center(bins: np.array) -> np.array:
+    return bins[:-1] + (bins[1]-bins[0])/2
+
+
+def hist_data(df: pd.Series, *,
+              bins: Union[int, None] = None, range: Union[int, None] = None,
+              errors: Union[str, None] = "normal",
+              label: str = "Data"):
+    h, bins = np.histogram(df, bins=bins, range=range)
+    if errors == "normal":
+        errors = np.sqrt(h)
+    else:
+        errors = None
+    plt.errorbar(get_bin_center(bins), h, fmt="r,",
+                 yerr=errors, capsize=2, label=label)
+
