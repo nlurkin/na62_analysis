4ccd6643a8ed9bfd17135336fe45d119e837628d
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-08-10 16:14:20 +0200

    Add documentation for the extract module

--- na62/extract.py
+++ na62/extract.py
@@ -1,4 +1,8 @@
-from typing import List
+"""
+This module contains function that extract parts of an original dataframe in "Full" format into a format that is
+more useful for manipulation of the individual parts.
+"""
+
 
 import numpy as np
 import pandas as pd
@@ -6,26 +10,52 @@ import pandas as pd
 from .constants import lkr_position
 
 
-def list_variables(df: pd.DataFrame) -> np.array:
-    return df.columns.values
-
-
 def print_event(df: pd.DataFrame, eventid: int) -> None:
+    """
+    Print the event with the given index without ellipsis
+
+    :param df: Dataframe
+    :param eventid: index of the event in the dataframe
+    """
     with pd.option_context('display.max_rows', None):
         print(df.loc[eventid])
 
 
 def track(df: pd.DataFrame, trackID: int) -> pd.DataFrame:
+    """
+    Extract all the variables related to the specified track from the dataframe in "Full" format.
+
+    :param df: Full dataframe
+    :param trackID: ID of the track to be extracted (1, 2 or 3)
+    :return: A new 'track' dataframe where the `track{trackID}_` prefix has been removed
+    """
+
     df = df.filter(like=f"track{trackID}")
     return df.rename(columns={_: _.replace(f"track{trackID}_", "") for _ in df})
 
 
 def cluster(df: pd.DataFrame, clusterID: int) -> pd.DataFrame:
+    """
+    Extract all the variables related to the specified cluster from the dataframe in "Full" format.
+
+    :param df: Full dataframe
+    :param clusterID: ID of the cluster to be extracted (1 or 2)
+    :return: A new 'cluster' dataframe where the `cluster{clusterID}_` prefix has been removed
+    """
     df = df.filter(like=f"cluster{clusterID}")
     return df.rename(columns={_: _.replace(f"cluster{clusterID}_", "") for _ in df})
 
 
 def photon_momentum(df: pd.DataFrame, clusterID: int) -> pd.DataFrame:
+    """
+    Extract a cluster from the "Full" dataframe and compute the momentum variables:
+     - direction_{x,y,z} = direction between position on LKr and vertex
+     - momentum_mag = LKr Energy
+
+    :param df: Full dataframe
+    :param clusterID: ID of the cluster to extract as momentum
+    :return: A new "momentum" dataframe
+    """
     c1 = cluster(df, clusterID)
     x = c1["position_x"] - df["vtx_x"]
     y = c1["position_y"] - df["vtx_y"]
@@ -39,6 +69,12 @@ def photon_momentum(df: pd.DataFrame, clusterID: int) -> pd.DataFrame:
 
 
 def all_tracks(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract all existing tracks from the "Full" dataframe and concatenate them in a single 'track' dataframe
+
+    :param df: Full dataframe
+    :return: A new concatenated 'track' dataframe containing all the existing tracks in the original dataframe
+    """
     t1 = track(df, 1)
     t2 = track(df, 2)
     t3 = track(df, 3)
@@ -47,6 +83,12 @@ def all_tracks(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def all_clusters(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract all existing cluster from the "Full" dataframe and concatenate them in a single 'cluster' dataframe
+
+    :param df: Full dataframe
+    :return: A new concatenated 'cluster' dataframe containing all the existing clusters in the original dataframe
+    """
     c1 = cluster(df, 1)
     c2 = cluster(df, 2)
 
@@ -54,6 +96,13 @@ def all_clusters(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def get_beam(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract the beam variables from the "Full" dataframe.
+
+    :param df: Full dataframe
+    :return: A new 'beam' dataframe where the `beam_` prefix has been removed
+    """
+
     beam = df.filter(like="beam_")
     beam = beam.rename(columns={_: _.replace("beam_", "")
                        for _ in beam.columns})
