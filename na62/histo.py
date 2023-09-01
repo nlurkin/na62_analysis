6045368910a513ad7ab76f3ff6b0892e780ced45
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-09-01 13:47:07 +0200

    Modify all histogram functions to work in two steps + entry point:
     - First step is to prepare a numpy histogram
     - Second step does the pyplot visual
     - Entry point calls First step then second step. Second step is skipped if global disable_plotting variable is set

--- na62/histo.py
+++ na62/histo.py
@@ -6,27 +6,22 @@ import pandas as pd
 
 from . import constants
 
+disable_plotting = False
+
+class Histogram:
+    def __init__(self, histogram, bins, entries):
+        self.histogram = histogram
+        self.bins = bins
+        self.entries = entries
+
+    def merge(self, other):
+        self.histogram =+ other.histogram
+        self.entries += other.entries
+
+        return self
 
 def get_bin_center(bins: np.array) -> np.array:
     return bins[:-1] + (bins[1]-bins[0])/2
-
-
-def hist_data(df: pd.Series, *,
-              bins: Union[int, None] = None, range: Union[int, None] = None,
-              errors: Union[str, None] = "normal",
-              label: str = "Data",
-              ax: Union[None, plt.Axes] = None) -> int:
-    h, bins = np.histogram(df, bins=bins, range=range)
-    if errors == "normal":
-        errors = np.sqrt(h)
-    else:
-        errors = None
-
-    if ax is None:
-        ax = plt
-    ax.errorbar(get_bin_center(bins), h, fmt="k,",
-                 yerr=errors, capsize=2, label=label)
-    return len(df)
 
 
 def compute_samples_weights(normalizations_dict: Dict[str, float]):
@@ -40,33 +35,113 @@ def compute_samples_weights(normalizations_dict: Dict[str, float]):
     return np.array(normalized_mc)
 
 
+################################################################
+# Histograming of Data
+################################################################
+
+def hist_data(df: pd.Series, *,
+              bins: Union[int, None] = None, range: Union[int, None] = None,
+              errors: Union[str, None] = "normal",
+              label: str = "Data",
+              ax: Union[None, plt.Axes] = None) -> Union[int, Histogram]:
+
+    h = prepare_hist_data(df, bins=bins, range=range)
+
+    if disable_plotting:
+        return h
+
+    return _hist_data(h, bins=bins, range=range, errors=errors, label=label, ax=ax)
+
+
+def prepare_hist_data(df: pd.Series, *,
+              bins: Union[int, None] = None, range: Union[int, None] = None) -> int:
+    h, bins = np.histogram(df, bins=bins, range=range)
+    h = Histogram(h, bins, len(df))
+
+    return h
+
+
+def _hist_data(histogram: Histogram, *,
+              bins: Union[int, None] = None, range: Union[int, None] = None,
+              errors: Union[str, None] = "normal",
+              label: str = "Data",
+              ax: Union[None, plt.Axes] = None) -> int:
+    if errors == "normal":
+        errors = np.sqrt(histogram.histogram)
+    else:
+        errors = None
+
+    if ax is None:
+        ax = plt
+    ax.errorbar(get_bin_center(histogram.bins), histogram.histogram, fmt="k,",
+                 yerr=errors, capsize=2, label=label)
+    return histogram.entries
+
+
+################################################################
+# Stacking of MC with Scaling to data
+################################################################
+
 def stack_mc_scale(dfs: List[pd.Series], *,
              bins: Union[int, None] = None, range: Union[int, None] = None,
              labels: Union[None, List[str]] = None,
              weights: Union[int, List[int]] = 1,
-             ndata: Union[None, int] = None,
-             ax: Union[None, plt.Axes] = None) -> Dict[str, int]:
+             ndata: int = 1,
+             ax: Union[None, plt.Axes] = None) -> Union[Dict[str, int], List[Histogram]]:
 
+    if not isinstance(dfs, List):
+        dfs = [dfs]
     if isinstance(weights, int):
         weights = [weights]*len(dfs)
-    if not labels:
-        labels = [None]*len(dfs)
 
     hlist = []
-    for df, weight, label in zip(dfs, weights, labels):
-        hlist.append((df, np.ones(shape=df.shape)*weight, label))
+    for df, weight in zip(dfs, weights):
+        h = prepare_for_stack_scale(df, bins=bins, range=range, weight=weight)
+        hlist.append(h)
 
-    hlist = sorted(hlist, key=lambda x: sum(x[1]))
-    sum_mc = sum([sum(_[1]) for _ in hlist])
+    if disable_plotting:
+        return hlist
+
+    return _stack_mc_scale(hlist, labels=labels, ndata=ndata, ax=ax)
+
+
+def prepare_for_stack_scale(df: pd.Series, *,
+             bins: Union[int, None] = None, range: Union[int, None] = None,
+             weight: int = 1) -> int:
+
+    weights = np.ones(shape=df.shape)*weight
+
+    h, bins = np.histogram(df, weights=weights, bins=bins, range=range)
+    h = Histogram(h, bins, sum(weights))
+
+    return h
+
+
+def _stack_mc_scale(hlist: List[Histogram], *,
+             labels: Union[None, List[str]] = None,
+             ndata: int = 1,
+             ax: Union[None, plt.Axes] = None) -> Dict[str, int]:
+
+    hlist = sorted(hlist, key=lambda x: x.entries)
+    sum_mc = sum([_.entries for _ in hlist])
+    sum_mc = 1 if sum_mc==0 else sum_mc
 
     if ax is None:
         ax = plt
 
-    ax.hist([_[0] for _ in hlist], weights=[_[1]*ndata/sum_mc for _ in hlist], bins=bins,
-             range=range, stacked=True, label=[_[2] for _ in hlist])
+    width = hlist[0].bins[1] - hlist[0].bins[0]
+    prev_bottom = 0
+    for h, label in zip(hlist, labels):
+        heights = h.histogram * ndata/sum_mc
+        ax.bar(get_bin_center(h.bins), heights, width=width, bottom=prev_bottom, label=label)
+        prev_bottom += heights
 
-    return {_[2]: sum(_[1]*ndata/sum_mc) for _ in hlist}
+    return {label: _.entries*ndata/sum_mc for _,label in zip(hlist, labels)}
 
+
+################################################################
+# Stacking of MC with Scaling to flux
+################################################################
 
 def stack_mc_flux(dfs: Dict[str, pd.Series], normalizations: Dict[str, int], *,
                   bins: Union[int, None] = None, range: Union[int, None] = None,
@@ -74,26 +149,56 @@ def stack_mc_flux(dfs: Dict[str, pd.Series], normalizations: Dict[str, int], *,
                   kaon_flux: Union[None, int] = None,
                   ax: Union[None, plt.Axes] = None) -> Dict[str, int]:
 
+    if not isinstance(dfs, dict):
+        dfs = {"dummy": dfs}
     if not labels:
         labels = [None]*len(dfs)
 
     hlist = []
+    upd_labels = []
     for sample, label in zip(dfs, labels):
         if label is None:
             label = sample
-        hlist.append((dfs[sample], np.ones(shape=dfs[sample].shape) *
-                     constants.kaon_br_map[sample]/normalizations[sample], label))
+        upd_labels.append(label)
+        h = prepare_for_stack_flux(dfs[sample], bins=bins, range=range)
+        hlist.append(h)
 
-    hlist = sorted(hlist, key=lambda x: sum(x[1]))
+    if disable_plotting:
+        return hlist
+
+    return _stack_mc_flux(hlist, normalizations, labels=upd_labels, kaon_flux=kaon_flux, ax=ax)
+
+
+def prepare_for_stack_flux(df: pd.Series, *,
+                  bins: Union[int, None] = None, range: Union[int, None] = None) -> Dict[str, int]:
+
+    weights = np.ones(shape=df.shape)
+    h, bins = np.histogram(df, weights=weights, bins=bins, range=range)
+    h = Histogram(h, bins, sum(weights))
+    return h
+
+
+def _stack_mc_flux(hlist: List[Histogram], normalizations: Dict[str, int], *,
+                  labels: Union[None, List[str]] = None,
+                  kaon_flux: Union[None, int] = None,
+                  ax: Union[None, plt.Axes] = None) -> Dict[str, int]:
 
     if ax is None:
         ax = plt
 
-    ax.hist([_[0] for _ in hlist], weights=[_[1]*kaon_flux for _ in hlist], bins=bins,
-             range=range, stacked=True, label=[_[2] for _ in hlist])
+    width = hlist[0].bins[1] - hlist[0].bins[0]
+    prev_bottom = 0
+    for h, label in zip(hlist, labels):
+        heights = h.histogram * kaon_flux * constants.kaon_br_map[label] / normalizations[label]
+        ax.bar(get_bin_center(h.bins), heights, width=width, bottom=prev_bottom, label=label)
+        prev_bottom += heights
 
-    return {_[2]: sum(_[1]*kaon_flux) for _ in hlist}
+    return {label: _.entries*kaon_flux for _,label in zip(hlist, labels)}
 
+
+################################################################
+# Other
+################################################################
 
 def plot_variations(x: List, y: List, nominal_cut: int, name: str, cut_name: str, ax: Union[None, plt.Axes]) -> None:
     """
